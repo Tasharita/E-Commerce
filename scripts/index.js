00e047b0da29e8cbc48180a2c3a5e38d17c73df0
Abe Mugwe <abraham.mugwe@strathmore.edu>
2025-06-22 19:11:36 +0300

    Search Functionality implemented

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const showcasecontainer=document.querySelector('.showcasecontainer')
 const singleproduct=document.querySelector('.single-product')
+const searchInput=document.getElementById('Search-bar')
+const searchButton=document.querySelector('.search')
 
 const fetchProducts=async()=>{
     const response=await fetch('https://fakestoreapi.com/products')
@@ -36,5 +38,36 @@ const fetchSingle= async(id)=>{
 
     localStorage.setItem('singleproduct',JSON.stringify(data))
 
-
 }
+const searchItem=async(search)=>{
+     const response=await fetch('https://fakestoreapi.com/products')
+    const data=await response.json()
+    console.log(data)
+
+    showcasecontainer.innerHTML='';
+
+    data.forEach((product)=>{
+        if(product.title.toLowerCase().includes(search.toLowerCase())){
+        const card=document.createElement('div');
+        card.innerHTML=`
+            <div>
+                <div class="card" style="width: 18rem;">
+                <div class="image-container"><img src="${product.image}" class="card-img-top" alt="..."></div>
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">${product.price*100} Ksh</p>
+                        <p class="product-rating">Customers have rated this: ${product.rating.rate}/5
+                        <button  onclick="fetchSingle(${product.id})" class="btn btn-primary">View Product</button>
+                        
+                    </div>
+                </div>
+            </div>`;
+        showcasecontainer.appendChild(card);
+    };
+    });
+};
+
+searchButton.addEventListener('click', () => {
+  const searchProduct = searchInput.value;
+  searchItem(searchProduct);
+});
