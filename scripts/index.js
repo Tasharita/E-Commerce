64d9219982184ae512d52d0a26c6dcccfe6c57ab
Abe Mugwe <abraham.mugwe@strathmore.edu>
2025-06-30 12:34:43 +0300

    Category implemented

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@ const singleproduct=document.querySelector('.single-product')
 const searchInput=document.getElementById('Search-bar')
 const searchButton=document.querySelector ('.search')
 const overlay=document.getElementById('overlay')
+const category=document.getElementById('cat')
+const category2=document.getElementById('cat2')
+
 
 const fetchProducts=async()=>{
     const response=await fetch('https://fakestoreapi.com/products')
@@ -88,3 +91,38 @@ searchInput.addEventListener('blur',()=>{
 window.addEventListener('scroll', () => {
   overlay.style.display = 'none';
 });
+
+//category-search
+const categoryClick=async(category)=>{
+     const response=await fetch('https://fakestoreapi.com/products')
+    const data=await response.json()
+    console.log(data)
+
+    showcasecontainer.innerHTML='';
+
+    data.forEach((product)=>{
+        if(product.category.toLowerCase().includes(category.toLowerCase())){
+        const card=document.createElement('div');
+        card.innerHTML=`
+            <div>
+                <div class="card" style="width: 18rem;">
+                <div class="image-container"><img src="${product.image}" class="card-img-top" alt="..."></div>
+                    <div class="card-body">
+                        <h5 class="card-title">${product.title}</h5>
+                        <p class="card-text">${product.price*100} Ksh</p>
+                        <p class="product-rating">Customers have rated this: ${product.rating.rate}/5
+                        <button  onclick="fetchSingle(${product.id})" class="btn btn-primary">View Product</button>
+                        
+                    </div>
+                </div>
+            </div>`;
+        showcasecontainer.appendChild(card);
+    };
+    });
+};
+
+category.addEventListener('click',()=>{
+    const catProduct=category.value;
+    categoryClick(catProduct);
+})
+
